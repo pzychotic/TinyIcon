9543afdb160c65801d8e479d21dd33954b15b488
Thomas Roß <pzycho1980@gmail.com>
2026-07-01 18:21:50 +0200

    [Modernize] Use primary constructor

--- Source/TinyIcon/ViewModels/ResolutionOptionViewModel.cs
+++ Source/TinyIcon/ViewModels/ResolutionOptionViewModel.cs
@@ -3,18 +3,12 @@ using CommunityToolkit.Mvvm.ComponentModel;
 namespace TinyIcon.ViewModels;
 
 /// <summary>A checkable resolution entry in the New Icon dialog.</summary>
-public partial class ResolutionOptionViewModel : ObservableObject
+public partial class ResolutionOptionViewModel(int size, bool isSelected) : ObservableObject
 {
-    public ResolutionOptionViewModel(int size, bool isSelected)
-    {
-        Size = size;
-        IsSelected = isSelected;
-    }
-
-    public int Size { get; }
+    public int Size { get; } = size;
 
     [ObservableProperty]
-    public partial bool IsSelected { get; set; }
+    public partial bool IsSelected { get; set; } = isSelected;
 
     public string Label => $"{Size}×{Size}";
 }
